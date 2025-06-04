50ec936d8fdc25dfd6fa4fe80cf08373ea326ec7
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-06-04 15:39:50 +0200

    add-style

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -2,20 +2,23 @@ import { Link } from "react-router-dom";
 
 export default function PostCard({ post }) {
   return (
-    <div className="card h-100">
-      <div className="card-header">
-        <h2>{post.title}</h2>
-      </div>
+    <div className="card h-100 shadow-sm rounded overflow-hidden">
       <img
-        className="img-fluid"
+        className="card-img-top object-fit-cover"
         src={`http://localhost:3000${post.image}`}
         alt={post.title}
       />
-      <div className="card-body mt-2">
+      <div className="card-header">
+        <h2>{post.title}</h2>
+      </div>
+
+      <div className="card-body d-flex flex-column justify-content-between">
         <p>{post.tags}</p>
       </div>
       <p>
-        <Link to={`/post/${post.id}`}>Mostra di più</Link>
+        <Link className="btn btn-primary mt-auto" to={`/post/${post.id}`}>
+          Mostra di più
+        </Link>
       </p>
     </div>
   );
--- src/pages/PostDetailPage.jsx
+++ src/pages/PostDetailPage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function PostDetailPage() {
   const [post, setPost] = useState({});
@@ -15,16 +16,33 @@ export default function PostDetailPage() {
 
   return (
     <div className="container">
-      <h2>{post.title}</h2>
-      <img
-        className="img-fluid mb-3"
-        src={`http://localhost:3000${post.image}`}
-        alt={post.title}
-      />
-      <p>
-        <em>{post.content}</em>
-      </p>
-      <p>{post.tags}</p>
+      <div className="row justify-content-center">
+        <div className="col-lg-8">
+          <div className="mb-4 text-center">
+            <h1 className="mb-3">{post.title}</h1>
+            <img
+              className="img-fluid rounded shadow-sm"
+              src={`http://localhost:3000${post.image}`}
+              alt={post.title}
+            />
+            <div className="mt-4">
+              <p>
+                <em>{post.content}</em>
+              </p>
+            </div>
+
+            <div>
+              <span className="badge bg-light text-dark me-2">{post.tags}</span>
+            </div>
+
+            <div className="text-end">
+              <Link to="/post" className="btn btn-outline-primary">
+                Torna ai post
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
